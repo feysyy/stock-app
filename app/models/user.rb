fb8661b453c03d7ed8768180adf49a18ceb74c5b
Fey Daniel <feysy97@gmail.com>
2022-12-16 00:15:50 +0800

    Add user role validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :role, inclusion: { in: %w(admin trader),
+         message: "%{value} is not a valid role" }
 
-  
   def status
     if self.approved
       return "Verified"
