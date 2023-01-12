cf4ce3d68bf5b2c927aaad558a22f36e3ef17ccc
Fey Daniel <feysy97@gmail.com>
2023-01-12 19:12:33 +0800

    Add user status validation

--- app/controllers/marketplace_controller.rb
+++ app/controllers/marketplace_controller.rb
@@ -1,4 +1,6 @@
 class MarketplaceController < ApplicationController
+  before_action :redirect_user_if_unauthorized
+
   def display
     @list = get_active_stocks
   end
@@ -7,6 +9,12 @@ class MarketplaceController < ApplicationController
 
   def get_active_stocks
     Fetcher.get_active_stocks
+  end
+
+  def redirect_user_if_unauthorized
+    unless current_user.approved?
+      redirect_to root_path, alert: "Sorry you are not yet approved"
+    end
   end
 
 end
