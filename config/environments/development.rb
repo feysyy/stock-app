bf47e6388514468eef77f090fc8de82441fee1f1
Fey Daniel <feysy97@gmail.com>
2023-01-14 01:22:59 +0800

    Update dotenv

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.smtp_settings = {
-    user_name:      ENV['app_email'],
-    password:       ENV['app_password'],
+    user_name:      ENV['APP_EMAIL'],
+    password:       ENV['APP_PASSWORD'],
     address:       'smtp.gmail.com',
     port:          587,
     authentication: "plain",
